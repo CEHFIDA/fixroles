b67a7f955952d2ab098132d186fd43a465f3a8da
Miroslav <lemmas.online@gmail.com>
2017-10-02 11:45:11 +0300

    small fix

---
+++ src/seed/CreateOrAttachAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Selfreliance\adminrole\AdminRoleController;
+use Selfreliance\fixroles\Models\Role;
+use App\User;
+
+class CreateOrAttachAdmin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	if(!AdminRoleController::checkExitsRole('Admin')) // if role is not created
+    	{
+            $adminRole = Role::create([
+                'name' => 'Admin',
+                'slug' => 'admin'
+            ]);
+
+            $user = User::findOrFail(1);
+            $user->attachRole($adminRole);
+    	}
+    }
+}
